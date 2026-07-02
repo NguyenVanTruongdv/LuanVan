b967aba06129cab9dbf449d265f31317eacdc4aa
Nguyen Van Truong <truong0236klj@gmail.com>
2026-07-03 00:06:01 +0700

    U

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d9064fa6cea936cfa45db35acf9dfe5a90dd41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437cdef05dacbb4ab17dbb06bed3c240781c3521")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
